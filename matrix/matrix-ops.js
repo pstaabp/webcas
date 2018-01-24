0508b97ed17ce7f2ce31f80f8b4e531251099bc8
Peter Staab <peter.staab@gmail.com>
2018-01-23 20:37:13 -0500

    update the mathjax cdn and the localStorage

--- matrix/matrix-ops.js
+++ matrix/matrix-ops.js
@@ -4,35 +4,37 @@ var currentStep = 1;
 var inputVisible = false;
 var $j = jQuery.noConflict();
 
-var results = new Array(); 
+var results = new Array();
 
 // Run the following code upon loading the document.
 
-$j(document).ready(function() 
+$j(document).ready(function()
     {
-      
+
       if (!('localStorage' in window && window['localStorage'] !== null))
         {alert("You need to update your browser. ");}
-        
+
         $j("#settings").css("display","none");
 	$j("#set-link").click(function() {$j("#settings").show("blind",null,"normal",null); return false;});
         $j("#help-link").click(function() {$j("#help").show("blind",null,"normal",null); return false;});
 	$j("#enter-matrix-button").click( function() {$j("#matrix-input").show();});
         $j("#store-matrix-button").click( function() { storeMatrix();});
         $j("#clear-matrix-button").click(function (){   $j("#matrix-entry").val("");});
-        
+
         var numKeys= localStorage.length;
         for(var i = 0; i < numKeys; i++)
         {
           var key = localStorage.key(i);
-	  if (key != "GEset"){
-          var m = new Matrix(localStorage.getItem(key).replace(/<br\/>/g,";").replace(/,/g," "));
-          storage.set(key,m);
-	  }
+          var match = /matrix\-(\w+)/.exec(key);
+          if(match){
+             var name = match[1];
+             var m = new Matrix(localStorage.getItem(key).replace(/<br\/>/g,";").replace(/,/g," "));
+             storage.set(name,m);
+      	  }
         }
-        
+
         updateMatrices();
-        
+
         createInputForm();
     });
 
@@ -41,24 +43,24 @@ $j(document).ready(function()
 
 function storeMatrix()
 {
-   var m; 
+   var m;
    try {
       m = new Matrix($j("#matrix-entry").val().replace(/\n/g,";").replace(/;*$/,""));
-           
-   } catch(err) 
+
+   } catch(err)
    {
       alert(err);
    }
-   
+
     var str = document.getElementById("matrix-name").value;
-    window.localStorage[str]=m;
+    window.localStorage["matrix-" +str]=m;
     storage.set(str,m);
-            
+
     updateMatrices();
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,"matrices"]);
     $j("#matrix-input").hide();
-     
-   
+
+
 }
 
 
@@ -66,9 +68,9 @@ function storeMatrix()
 
 function updateMatrices()
 {
-   
+
    $j("#matrices").html("");
-   
+
    for(var i = 0; i < storage.keys().length; i++)
    {
       var key = storage.keys()[i];
@@ -77,40 +79,40 @@ function updateMatrices()
       str += "\\( " + key +"=" + (m.toLaTeX()) + " \\)</td>";
       str += "<td><button id='edit-matrix-button-" + (i) +"'>Edit</button></td></tr>";
       str += "<tr><td><button id='delete-matrix-button-" + (i) +"'>Delete</button></td></tr></table></div>";
-   
+
       $j("#matrices").append(str);
-      
-      
+
+
       // Need to define what happens on button clicks here.
-      
+
       $j("#edit-matrix-button-"+i).click( function() {editMatrix(Number(this.id.split("-")[3]));
       });
-      
+
       $j("#delete-matrix-button-"+i).click( function() {
          var j = Number(this.id.split("-")[3]);
          var varname = storage.keys()[j];
          localStorage.removeItem(varname);
          storage.unset(varname);
          updateMatrices();
-         
+
          });
       MathJax.Hub.Queue(["Typeset",MathJax.Hub,"right"]);
-      
+
    }
-   
-   
-   
-   
+
+
+
+
 }
 
 
 function createInputForm()
 {
    $j("#left-output").append("<div id='input-box'>Input:<input id='input-text' type='text' size='30'/><button id='input-enter'>Enter</button></div>")
-   
+
    $j("#input-enter").click( function () { parseInput($j("#input-text").val());});
    $j("#input-text").keypress(function (e) {if (e.keyCode == 13) parseInput($j("#input-text").val());});
-   
+
 }
 
 var detRE = /(det|inv|rowreduce)\((\w+)\)/;
@@ -119,18 +121,18 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
  function parseInput(str)
 {
    var outputStr="";
-   
+
    try
    {
       var scalarMult;
-      
+
       if (detRE.test(str))
       {
          var opName = detRE.exec(str)[1];
          var varName = detRE.exec(str)[2];
          var m = storage.get(varName);
          var matty = null;
-	 
+
          switch (opName)
          {
          case "det":
@@ -146,8 +148,8 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
             outputStr = "\\[ \\mbox{rowReduce}("+varName+")=" + matty.toLaTeX() + "\\]";
             break;
          }
-         results[results.length]=matty; 
-         
+         results[results.length]=matty;
+
       } else if (matOpRE.test(str))
       {
          var ops = matOpRE.exec(str);
@@ -155,14 +157,14 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
          var m1 = null;
          var num1 = null;
 	 var brackets=/\[(\d+)\]/;
-         
+
 	 var br1=brackets.exec(ops[1]);
 	 var br2=brackets.exec(ops[3]);
-	 
+
          try {
             num1 = Mnumber.parseConstant(var1);
             scalarMult = true;
-         }  
+         }
          catch (err)
          {
 	  if(brackets.exec(ops[1]) != null)
@@ -170,19 +172,19 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
 	    var i = parseInt(brackets.exec(ops[1])[1]);
 	    m1 = results[i-1];
 	  } else {
-	      
+
             m1 = storage.get(var1);
-            
+
             if (m1 == null)
                alert("Matrix " + var1 + " is not defined.");
-            
+
             scalarMult = false;
-	    
+
 	  }
-         } 
-         
+         }
+
          var operation = ops[2];
-         
+
          var var2 = ops[3];
 	 var m2 = null;
 	 if(brackets.exec(ops[3]) != null)
@@ -194,12 +196,12 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
 	    if ((/\[a-zA-Z]+/.test(var2)) && (m2 == null))
 	       alert("Matrix " + var2 + " is not defined.");
 	  }
-         
-         
-         
+
+
+
          var result, opout;
          var matty;
-	 
+
          switch(operation)
          {
          case "+":
@@ -213,33 +215,33 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
          case "*":
             if (scalarMult)
 	    {
-	       matty = m2.times(num1); 
+	       matty = m2.times(num1);
                outputStr = "\\[" + (num1.toLaTeX()) + var2 + "=" + (matty.toLaTeX()) + "\\]";
 	    }
             else
 	    {
-	       matty = m1.times(m2); 
+	       matty = m1.times(m2);
                outputStr = "\\[" + var1 + var2 + "=" + (matty.toLaTeX()) + "\\]";
 	    }
             break;
          case "^":
-            var pow = parseInt(var2); 
+            var pow = parseInt(var2);
             if(isNaN(pow))
                throw var2 + " is not an integer";
             else
 	    {
 	      matty = m1.power(pow);
-               outputStr = "\\[" + var1 + "^{"+pow +"}=" + (matty.toLaTeX()) + "\\]";            
+               outputStr = "\\[" + var1 + "^{"+pow +"}=" + (matty.toLaTeX()) + "\\]";
 	    }
-         } 
-         results[results.length]=matty; 
+         }
+         results[results.length]=matty;
       }else
       {
       alert("Error in input");
       return;
       }
       var tableRow = "<tr><td class='lcol'>" + outputStr + "</td><td class='rcol'> ["+ currentStep + "] </td></tr>";
-      currentStep++; 
+      currentStep++;
 
       $j("#tab").append(tableRow);
       MathJax.Hub.Queue(["Typeset",MathJax.Hub,"tab"]);
@@ -250,8 +252,8 @@ var matOpRE = /(\[\d+\]|\w+|-?\d+|\(-?\d+\/\d+\))([\+\-\*\^])(\[\d+\]|\w+|\d+)/
       alert(er);
       return;
    }
-   
-   
+
+
 }
 
 function editMatrix(num)
@@ -261,6 +263,3 @@ function editMatrix(num)
    $j("#matrix-name").val(matrixName);
    $j("#matrix-entry").val(storage.get(matrixName).toPlainText());
 }
-
-
-
